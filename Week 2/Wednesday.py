3287c08aae6dd17df71d146045dfd6b7308cd1d8
bss233 <bss233@nau.edu>
2019-01-28 12:23:20 -0700

    Finished Wednesday's solutions

--- Week 2/Wednesday.py
+++ Week 2/Wednesday.py
@@ -8,11 +8,38 @@ def simpleInterest(start, years, rate):
 
 #Exercise 2
 def minCoins(amount):
-    wholeNum = int(amount // 1)
-    if amount == wholeNum:
-        return amount // .25
-    else:
-        cents = round((amount % 1), 2)
-        quarters = wholeNum // .25
-        if (cents * 100) % 25 == 0:
-            
+    runTot = 100*amount
+    
+    # create variable placeholders in case if statements aren't used
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+
+    # if we have more than a quarter...
+    if runTot >= 25:
+        # find number of quarters
+        quarters = (runTot // 25)  
+        # could also use -= here
+        runTot = runTot - (quarters * 25)
+
+    # if we have more than a dime
+    if runTot >= 10:
+        # find number of dimes. could also use += here
+        dimes = (runTot // 10)
+        runTot = runTot - (dimes * 10)
+    
+    # if we have more than a nickel
+    if runTot >= 5:
+        # find number of nickels. could also use += here
+        nickels = (runTot // 5)
+        runTot = runTot - (nickels * 5)
+
+    if runTot >= 1:
+        # find number of pennies. could also use += here
+        pennies = (runTot / 1)
+        runTot = runTot - (pennies * 1)
+    
+    coins = quarters + dimes + nickels + pennies
+    return int(coins)
+        
